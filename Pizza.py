a23eb9b6c0d26c55cb87f41b3dd35a4bf0200eae
Guillen <cgarciaguillen@yahoo.es>
2020-03-10 14:09:10 +0100

    Primeros pasos

--- Pizza.py
+++ Pizza.py
@@ -1,4 +1,30 @@
 
 
-M=17
-N=4
+def Pizza(M,N,S):
+    if M>=sum(S):
+        K=N
+        L=S
+        return (K,L)
+    
+    else:
+        for i in range(len(S)):#Numero de pizzas
+            for j in S: #1 pizza
+                if j==M:
+                    K=1
+                    L=[S.index(j)]
+                    return(K,L)
+                
+ 
+
+
+
+M=6 #Max slices
+N=4 #Max pizzas
+S = [2, 5, 6, 8] #Slices per pizza
+           
+print(Pizza(M,N,S))              
+                
+            
+
+#print("K=",K)
+#print("L=",L)
